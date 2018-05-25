d101d3073c50e8a3f3c8fc240fac49d4a40718d5
Bill Glover <bill@billglover.com>
2018-05-25 04:50:28 -0500

    Just put the normalized data in that Activity.js too for now.

--- redux/Actions.js
+++ redux/Actions.js
@@ -1,67 +1,89 @@
 export const DATA_AVAILABLE = 'DATA_AVAILABLE';
 export const ADD_READING = 'ADD_READING';
 
-const sitesById = {
-  '1': {
-    id: '1',
-    name: 'Markarth',
-    location: {latitude: 31.8767366, longitude: -102.4135945},
-    description: "Site of an old Dwemer city."
-  },
-  '2': {
-    id: '2',
-    name: 'Riften',
-    location: {latitude: 35.2021668, longitude: -101.9450282},
-    description: "Once a bustling trade hub, now a decaying backwater full of thieves."
-  },
-  '3': {
-    id: '3',
-    name: 'Riverwood',
-    location: {latitude: 39.2188492, longitude: -121.0887236},
-    description: "A beautiful, sleepy mountain village."
-  },
-};
+// Not sure why this doesn't work.
+// import sample from '../config/Data';
 
-const machinesById = {
-  '5': {
-    site: '2',
-    id: '5',
-    name: 'Water Well',
-  },
-  '6': {
-    site: '2',
-    id: '6',
-    name: 'Ratway',
-  },
-};
+// So we'll just include it here.
+const normalized = {
+  entities: {
+    sites: {
+      byId: {
+        '1': {
+          id: '1',
+          name: 'Markarth',
+          location: {latitude: 31.8767366, longitude: -102.4135945},
+          description: "Site of an old Dwemer city."
+        },
+        '2': {
+          id: '2',
+          name: 'Riften',
+          location: {latitude: 35.2021668, longitude: -101.9450282},
+          description: "Once a bustling trade hub, now a decaying backwater full of thieves."
+        },
+        '3': {
+          id: '3',
+          name: 'Riverwood',
+          location: {latitude: 39.2188492, longitude: -121.0887236},
+          description: "A beautiful, sleepy mountain village."
+        },
+      },
+      allIds: ['1', '2', '3']
+    },
+    machines: {
+      byId: {
+        '5': {
+          site: '2',
+          id: '5',
+          name: 'Water Well',
+        },
+        '6': {
+          site: '2',
+          id: '6',
+          name: 'Ratway',
+        },
+      },
+      allIds: ['5', '6']
+    },
+    points: {
+      byId: {
+        '11': {
+          machine: '5',
+          id: '11',
+          name: 'Dipstick',
+          unit: 'feet'
+        },
+        '12': {
+          machine: '5',
+          id: '12',
+          name: 'Salinity Probe',
+          unit: 'ppm'
+        },
+      },
+      allIds: ['11', '12']
+    },
+    readings: {
+      byId: {
+      },
+      allIds: []
+    },
+  }
+}
 
-const pointsById = {
-  '11': {
-    machine: '5',
-    id: '11',
-    name: 'Dipstick',
-    unit: 'feet'
-  },
-  '12': {
-    machine: '5',
-    id: '12',
-    name: 'Salinity Probe',
-    unit: 'ppm'
-  },
-};
 
-const sample = {
-  sitesById: sitesById,
-  machinesById: machinesById,
-  pointsById: pointsById,
-  sites: ['1', '2', '3'],
-  machines: ['5', '6'],
-  points: ['11', '12'],
-  newReadings: []
+export const sample = {
+  sitesById: normalized.entities.sites.byId,
+  machinesById: normalized.entities.machines.byId,
+  pointsById: normalized.entities.points.byId,
+  sites: normalized.entities.sites.allIds,
+  machines: normalized.entities.machines.allIds,
+  points: normalized.entities.points.allIds,
+  newReadings: normalized.entities.readings.allIds
 };
 
 
 export function getData() {
+  console.log('Actions.js sample: ' + sample);
   return dispatch => {
     //Make API Call
     //delay the retrieval [Sample reasons only]
