e309560f4010a117484a2635d32fd84e84831b13
Bill Glover <bill@billglover.com>
2018-05-27 17:23:06 -0500

    Check-in the updated tests.

--- __tests__/Activity-test.js
+++ __tests__/Activity-test.js
@@ -1,3 +1,4 @@
+import * as Data from '../config/Data';
 import * as Actions from '../redux/Actions';
 
 it('dispatches ADD_READING', () => {
@@ -5,14 +6,14 @@ it('dispatches ADD_READING', () => {
 });
 
 it('retrieves machines by site', () => {
-  expect( Actions.ids_by_site(Actions.sample.entities.machines.byId)['2'].sort()).toEqual(['5','6']);
+  expect( Data.ids_by_site(Actions.sample.entities.machines.byId)['2'].sort()).toEqual(['5','6']);
 });
 
 it('retrieves points by machine', () => {
-  expect( Actions.ids_by_machine(Actions.sample.entities.points.byId)['5'].sort()).toEqual(['11','12']);
+  expect( Data.ids_by_machine(Actions.sample.entities.points.byId)['5'].sort()).toEqual(['11','12']);
 });
 
 it('retrieves readings by point', () => {
-  expect( Actions.ids_by_point(Actions.sample.entities.newReadings.byId)['12']).toBe(undefined);
-  expect( Actions.ids_by_point(Actions.sample.entities.newReadings.byId)['11'].sort()).toEqual(['21','22']);
+  expect( Data.ids_by_point(Actions.sample.entities.newReadings.byId)['12']).toBe(undefined);
+  expect( Data.ids_by_point(Actions.sample.entities.newReadings.byId)['11'].sort()).toEqual(['21','22']);
 });
