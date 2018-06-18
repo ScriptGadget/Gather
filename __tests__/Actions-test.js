f563c0e9e7bb2ab0661eed869744b3fd91cdedd3
Bill Glover <bill@billglover.com>
2018-06-18 13:07:59 -0500

    Keep test data in Data.js instead.

--- __tests__/Actions-test.js
+++ __tests__/Actions-test.js
@@ -5,17 +5,16 @@ it("dispatches ADD_READING", () => {
   expect(Actions.addReading("some reading")((x) => x)).toEqual({ type: Actions.ADD_READING, reading: "some reading" });
 });
 
-
 it("retrieves machines by site", () => {
-  expect( Data.ids_by_site(Actions.sample.entities.machines.byId)[2].sort()).toEqual([2, 3]);
+  expect( Data.ids_by_site(Data.sample.entities.machines.byId)[2].sort()).toEqual([2, 3]);
 });
 
 it("retrieves points by machine", () => {
-   expect( Data.ids_by_machine(Actions.sample.entities.points.byId)[3].sort()).toEqual([3, 4]);
+   expect( Data.ids_by_machine(Data.sample.entities.points.byId)[3].sort()).toEqual([3, 4]);
 });
 
 it("retrieves history by point", () => {
-  expect( Data.ids_by_point(Actions.sample.entities.readings.byId)[12]).toBe(undefined);
-  expect( Data.ids_by_point(Actions.sample.entities.readings.byId)[1].sort()).toEqual([1, 2]);
-  expect( Data.ids_by_point(Actions.sample.entities.readings.byId)[4].sort()).toEqual([7, 8]);  
+  expect( Data.ids_by_point(Data.sample.entities.readings.byId)[12]).toBe(undefined);
+  expect( Data.ids_by_point(Data.sample.entities.readings.byId)[1].sort()).toEqual([1, 2]);
+  expect( Data.ids_by_point(Data.sample.entities.readings.byId)[4].sort()).toEqual([7, 8]);  
 });
