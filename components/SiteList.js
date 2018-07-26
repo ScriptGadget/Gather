46585e838a264c775762e230abc5cd11d52a818c
Bill Glover <bill@billglover.com>
2018-07-26 15:58:36 -0500

    Show a message if there's no Route for this Pumper.

--- components/SiteList.js
+++ components/SiteList.js
@@ -26,7 +26,7 @@ class SiteList extends Component {
 
   render() {
     const data = this.props.data;
-    
+
     if (this.props.loading) {
       return (
         <View style={styles.activityIndicatorContainer}>
@@ -34,44 +34,55 @@ class SiteList extends Component {
         </View>
       );
     } else {
+      if (data.entities.sites.allIds.length < 1) {
+        return (
+            <ScrollView>
+              <Text style={{ alignSelf: 'stretch', height: 200, padding: 50 }}>
+                No Route Found. Please contact your operator to make sure you have been assigned a route.
+              </Text>
+            </ScrollView>
+        );
+      }
+      else
+      {
+        return (
+          <ScrollView>
+            <MapView
+              style={{ alignSelf: 'stretch', height: 200 }}
+              region={{
+              latitude: 35.12,
+              longitude: -102.01,
+              latitudeDelta: 3,
+              longitudeDelta: 3,
+              }}>
 
-      return (
-        <ScrollView>
-          <MapView
-          style={{ alignSelf: 'stretch', height: 200 }}
-          region={{
-          latitude: 35.12,
-          longitude: -102.01,
-          latitudeDelta: 3,
-          longitudeDelta: 3,
-        }}>
+              {data.entities.sites.allIds.map(site => (
+                <MapView.Marker
+                  key={site}
+                  coordinate={{
+                    latitude: data.entities.sites.byId[site].location.lat,
+                    longitude: data.entities.sites.byId[site].location.lng,
+                  }}
+                  title={`${data.entities.sites.byId[site].name}`}
+                  description={`${data.entities.sites.byId[site].description}`}
+                />
+              ))}
+            </MapView>
 
-        {data.entities.sites.allIds.map(site => (
-          <MapView.Marker
-            key={site}
-            coordinate={{
-              latitude: data.entities.sites.byId[site].location.lat,
-              longitude: data.entities.sites.byId[site].location.lng,              
-            }}
-            title={`${data.entities.sites.byId[site].name}`}
-            description={`${data.entities.sites.byId[site].description}`}
-          />
-        ))}
-        </MapView>
+            <List>
+              {data.entities.sites.allIds.map(site => (
+                <ListItem
+                  key={site}
+                  leftIcon={{ name: 'place' }}
+                  title={`${data.entities.sites.byId[site].name}`}
+                  onPress={() => this.onLearnMore(data)(site)}
+                />
+              ))}
+            </List>
 
-          <List>
-          {data.entities.sites.allIds.map(site => (
-            <ListItem
-              key={site}
-              leftIcon={{ name: 'place' }}
-              title={`${data.entities.sites.byId[site].name}`}
-            onPress={() => this.onLearnMore(data)(site)}
-            />
-          ))}
-        </List>
-          </ScrollView>
+        </ScrollView>
       );
-      
+      }
     }
   }
 }
